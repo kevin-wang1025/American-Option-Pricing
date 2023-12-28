bbf7b2fefcb6897e1f3bbbb9209481c78ca7123f
kevin-wang1025 <85164758+kevin-wang1025@users.noreply.github.com>
2023-12-29 02:18:26 +0800

    BAW

--- BAW.m
+++ BAW.m
@@ -7,14 +7,14 @@ b = 0.03;
 sigma = 0.3;
 T = 1;
 
-[d1, d2, european_price] = bs_model(S, K, T, r, b, sigma);
-[q2, S_star] = Newton(S, 1e-1, 10000);
+[d1, european_price] = bs_model(S, K, T, r, b, sigma);
+[q2, S_star] = Newton(1e-10, 10000);
 d1_star = (log(S_star/K)+(b+1/2*sigma^2)*T) / (sigma*sqrt(T));
 
 american_price = 0;
 if S<S_star
-    premium = (S_star/q2)*(1-exp((b-r)*T)*normcdf(d1))*(S/S_star)^q2;
-    disp(premium);
+    premium = (S_star/q2)*(1-exp((b-r)*T)*normcdf(d1_star))*(S/S_star)^q2;
+    fprintf('美式溢價: %d\n',premium);
     american_price = european_price + premium;
 else
     american_price = S - K;
--- Newton.m
+++ Newton.m
@@ -1,5 +1,6 @@
 % S0=100, K=100, r=0.05, T=1, sigma=0.3, step=252, n=1000
-function [q2, result] = Newton(initialGuess, tol, maxIter)
+%用牛頓法求解臨界價格
+function [q2, result] = Newton(tol, maxIter)
 
     %手動調整參數
     r = 0.05;
@@ -11,26 +12,39 @@ function [q2, result] = Newton(initialGuess, tol, maxIter)
     %定義目標函數 
     M = 2*r/sigma^2;
     N = 2*b/sigma^2;
-    q2 = (-N+1+sqrt((N-1)^2+4*M/K)) / 2;
+    X = 1-exp(-r*T);
+    q2 = (-N+1+sqrt((N-1)^2+4*M/X)) / 2;
     
-    f = @(S) S - K - bs_model(S, 100, 1, 0.05, 0.03, 0.3) - (1-exp((b-r)*T)*...
-        normcdf((log(S/K)+(b+1/2*sigma^2)*T) / (sigma*sqrt(T))))*S/q2;
+    %f = @(S) S - K - bs_model(S, K, T, r, b, sigma) - (1-exp((b-r)*T)*...
+    %    normcdf((log(S/K)+(b+1/2*sigma^2)*T) / (sigma*sqrt(T))))*S/q2;
+    
+    %最佳起始值
+    q2i = (-N+1+sqrt((N-1)^2+4*M)) / 2;
+    Si = K/(1-1/q2i);
+    h2 = -1*(b*T+2*sigma*sqrt(T))*(K/(Si-K));
+    S = K + (Si-K)*(1-exp(h2)); %起始值
+    fprintf('起始值: %d\n', S);
+    [d1, c] = bs_model(S, 100, 1, 0.05, 0.03, 0.3);
+    lhs = S - K;
+    rhs = c + (1-exp((b-r)*T)*normcdf(d1))*S/q2;
+    bi = exp((b-r)*T)*normcdf(d1)*(1-1/q2) + (1-exp((b-r)*T)*normpdf(d1)/(sigma*sqrt(T)))/q2;
     
     %迭代求解過程
-    S = initialGuess;
     iteration = 0;
-    
-    while abs(f(S))/K>tol && iteration<maxIter
-        [d1, d2, c] = bs_model(S, 100, 1, 0.05, 0.03, 0.3);
+    while abs(lhs-rhs)/K>tol && iteration<maxIter
+        fprintf('第%d次迭代 ',iteration+1);
+        S = (K+rhs-bi*S)/(1-bi);
+        [d1, c] = bs_model(S, 100, 1, 0.05, 0.03, 0.3);
+        lhs = S - K;
+        rhs = c + (1-exp((b-r)*T)*normcdf(d1))*S/q2;
         bi = exp((b-r)*T)*normcdf(d1)*(1-1/q2) + (1-exp((b-r)*T)*normpdf(d1)/(sigma*sqrt(T)))/q2;
-        S = (K+c-bi*S)/(1-bi);
-        %disp(abs(f(S))/K);
+        
+        fprintf('Error: %d\n',abs(lhs-rhs)/K);
         iteration = iteration + 1;
     end 
     
-    disp(S);
+    fprintf('臨界股價: %d',S);
     result = S;
 end
 
-%Newton(100, 1e-1, 100000)
-%138.8884 
+%Newton(1e-10, 10000)
